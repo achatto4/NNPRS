90a69da4f8d8b456c94b278f4bf756be83f8f029
Anagh Chattopadhyay <achatto4@jh.edu>
2025-02-22 20:07:57 -0500

    trial(2)

--- PRS_new.R
+++ PRS_new.R
@@ -104,9 +104,9 @@ if ( opt$verbose >= 1 ) cat("\n** Step 1. Preprocessing data **\n")
 results <- data.frame(iter = integer(), eta = numeric(), alpha = numeric(), R2 = numeric())
 
 # Define parameter grids
-iters <- c(100)
+iters <- c(1000)
 etas <- c(0.0001)  # Use same eta for all
-alphas <- c(0.1)  # Use same alpha for all
+alphas <- c(0.01)  # Use same alpha for all
 
 for (iter in iters) {
   for (eta in etas) {
@@ -302,7 +302,7 @@ ff <- foreach(j = 1:length(allchrom), ii = icount(), .final = function(x) NULL) 
     eta_l = eta, #Use ADAM
     eta_m = eta,
     max_iter = iter,
-    adaptive = FALSE,
+    adaptive = TRUE,
     eta = 0.001,
     beta1 = 0.9,
     beta2 = 0.999,

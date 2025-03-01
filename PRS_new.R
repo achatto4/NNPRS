4813c7c486391561c8e45aacb7f85105c63df9b2
Anagh Chattopadhyay <achatto4@jh.edu>
2025-03-01 01:07:13 -0500

    .

--- PRS_new.R
+++ PRS_new.R
@@ -109,9 +109,9 @@ results <- data.frame(iter = integer(), eta = numeric(), alpha = numeric(), R2 =
 # alphas <- c(0.1, 0.01, 0.001, 0)  # Use same alpha for all
 
 # Define parameter grids (non-ADAM)
-iters <- c(1)
-etas <- c(1)  # Use same eta for all
-alphas <- c(1)  # Use same alpha for all
+iters <- c(100)
+etas <- c(0.0001)  # Use same eta for all
+alphas <- c(0.01)  # Use same alpha for all
 
 for (iter in iters) {
   for (eta in etas) {
@@ -307,7 +307,7 @@ ff <- foreach(j = 1:length(allchrom), ii = icount(), .final = function(x) NULL) 
     eta_l = eta, #Use ADAM
     eta_m = eta,
     max_iter = iter,
-    adaptive = TRUE,
+    adaptive = FALSE,
     eta = 0.001,
     beta1 = 0.9,
     beta2 = 0.999,

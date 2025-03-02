c939519a3617c106c80170bd0dcb7cd883c183e5
Anagh Chattopadhyay <achatto4@jh.edu>
2025-03-02 05:30:43 -0500

    .

--- PRS_new.R
+++ PRS_new.R
@@ -105,7 +105,7 @@ results <- data.frame(iter = integer(), eta = numeric(), alpha = numeric(), R2 =
 
 # Define parameter grids (non-ADAM)
 q_thresh = 0
-iters <- c(10, 100, 1000)
+iters <- c(100, 1000, 10)
 #etas <- c(0.01, 0.001, 0.1, 0.0001)  # Use same eta for all
 alphas <- c(0.001, 0.01, 0.1)  # Use same alpha for all
 
